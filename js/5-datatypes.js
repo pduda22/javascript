e5b5b2a288e152c64deb4a5bd2f7c2e96eec6253
Piotr Duda <p.duda_22@wp.pl>
2017-12-17 12:15:29 +0100

    data types

---
+++ js/5-datatypes.js
@@ -0,0 +1,27 @@
+'use strict';
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWplata;
+
+calkowitaWplata = wyplata + premia;
+
+console.log(calkowitaWplata);
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWplataStr;
+
+calkowitaWplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWplataStr);
+
+
+var czyJestSmog;
+czyJestSmog = false ;
+
+if (czyJestSmog) {
+    console.log("jest SMOG");
+    
+} else {
+    console.log("nie ma SMOGa ;)");
+}
