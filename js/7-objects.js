5a93969d28659f0c7b2f4dfadeeda2e0c767e15c
Piotr Duda <p.duda_22@wp.pl>
2017-12-17 14:32:53 +0100

    math

--- js/7-objects.js
+++ js/7-objects.js
@@ -1,22 +1,62 @@
 'use strict';
 
-var kaja = {
-    imie: "Kaja",
-    wzrost: 152,
-    przedstawOsobe: function() {console.log(this.imie)}
-}
+//var kaja = {
+//    imie: "Kaja",
+//    wzrost: 152,
+//    przedstawOsobe: function() {console.log(this.imie)}
+//}
+//
+//var krystian = {
+//    imie: "Krystian",
+//    wzrost: 180,
+//    przedstawOsobe: function() {console.log(this.imie)}
+//}
+//
+//kaja.przedstawOsobe();
+//krystian.przedstawOsobe();
+//
+//krystian.waga = 80;
+//krystian.wyswietlDetale = function() {
+//    console.log(this.imie + "," + this.wzrost + "," + this.waga)
+//}
+//krystian.wyswietlDetale();
 
-var krystian = {
-    imie: "Krystian",
-    wzrost: 180,
-    przedstawOsobe: function() {console.log(this.imie)}
+class Osoba {
+    
+    constructor(imie, nazwisko, plec) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.plec = plec;     
+    }
+    
+    wyswietlDetale() {
+        console.log("Imię:" + this.imie + " Nazwisko:" + this.nazwisko + " Płeć:" + this.plec);
+    }
 }
+var ewa = new Osoba("Ewa" , "Rogulska", "Kobieta");
+ewa.wyswietlDetale();
 
-kaja.przedstawOsobe();
-krystian.przedstawOsobe();
+var damian = new Osoba("Damian", "Pawelec", "Mężczyzna")
+damian.wyswietlDetale();
+console.log(ewa);
 
-krystian.waga = 80;
-krystian.wyswietlDetale = function() {
-    console.log(this.imie + "," + this.wzrost + "," + this.waga)
-}
-krystian.wyswietlDetale();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
