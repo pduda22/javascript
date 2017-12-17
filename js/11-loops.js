be5d547324e54af25bfb9480f47cc2a1e9bbf7fd
Piotr Duda <p.duda_22@wp.pl>
2017-12-17 16:33:10 +0100

    json

--- js/11-loops.js
+++ js/11-loops.js
@@ -20,6 +20,8 @@ while ( it < tablica.length ) {
     it++;
 }
 
+
+
 var iter = 2;
 do {
     iter++;
@@ -27,3 +29,33 @@ do {
     
 } while (iter <5 )
     
+    
+    
+    var a = 0;
+    while (a < 10) {
+        console.log(++a);
+        if (a == 5) {
+            break;
+        }
+    }
+
+
+
+for ( var b = 0; b < 5 ; ++b ) {
+    if ( b == 2 ) {
+        continue;
+        
+    } else {
+        console.log(b);
+    }
+}
+
+
+
+
+
+
+
+
+
+
