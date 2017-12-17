0a23c6f0e0c995f1f7b939ac2e50339c4d2951e4
Piotr Duda <p.duda_22@wp.pl>
2017-12-17 14:55:59 +0100

    operacje

---
+++ js/9-operators.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var liczba1 = 13, liczba2 = 5;
+
+var wynik;
+
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+liczba1 = liczba1 + liczba2;
+
+console.log(liczba1);
+
+if ("2" === 2) {
+    console.log("prawda");
+} else {
+    console.log("fałsz");
+}
+
+if (!( ("2" !== 2) && (8 < 4) ) ) {
+    document.write("<br> prawda");
+}
+
